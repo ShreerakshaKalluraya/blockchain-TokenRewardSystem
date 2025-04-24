9809a2ebc11c67dc78ebea02ca29dbbe5a4b6ddb
ShreerakshaKalluraya <shreerakshabaddadka@gmail.com>
2025-04-25 02:12:36 +0530

    Removed_Unnecessary_files

--- Contracts/LoyaltyToken.sol
+++ Contracts/LoyaltyToken.sol
@@ -1,78 +1,181 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract LoyaltyToken {
-    string public name = "LoyaltyToken";
-    string public symbol = "LTY";
-    uint8 public decimals = 18;
-    uint public totalSupply;
-
-    mapping(address => uint) public balanceOf;
-    mapping(address => bool) public minters;
-    mapping(address => string) public registeredBusinessNames; // Only store business names
-    mapping(address => bool) public isRegisteredBusiness; // Track if an address is a registered business
-
+contract BusinessVoucher {
+    // State variables
     address public admin;
+    
+    // Business registration tracking
+    mapping(address => bool) public isRegisteredBusiness;
+    mapping(address => string) public businessNames;
+    address[] public registeredBusinesses;
+    
+    // Voucher tracking
+    struct Voucher {
+        uint id;
+        string description;
+        uint value;
+        bool used;
+        address business;
+        uint issueDate;
+    }
+    
+    // Customer vouchers mapping: customer address => voucher ID => Voucher
+    mapping(address => mapping(uint => Voucher)) public customerVouchers;
+    mapping(address => uint) public customerVoucherCount;
+    
+    // Public voucher tracking
+    struct PublicVoucher {
+        uint id;
+        string description;
+        uint value;
+        address business;
+        uint totalSupply;
+        uint claimedCount;
+        bool isActive;
+    }
+    
+    PublicVoucher[] public publicVouchers;
+    mapping(uint => mapping(address => bool)) public hasClaimedVoucher;
 
-    // Event for business registration
-    event BusinessRegistered(address indexed businessAddress, string businessName);
+    // Events
+    event BusinessRegistered(address indexed businessAddress, string name);
+    event VoucherIssued(address indexed business, address indexed customer, uint voucherId, uint value);
+    event VoucherUsed(address indexed customer, uint voucherId);
+    event PublicVoucherCreated(uint indexed voucherId, address indexed business, string description, uint value);
+    event VoucherClaimed(address indexed customer, uint indexed voucherId);
     
     constructor() {
         admin = msg.sender;
-        minters[msg.sender] = true; // Admin is a minter by default
     }
-
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can call this");
-        _;
-    }
-
-    modifier onlyMinter() {
-        require(minters[msg.sender], "Only minters can call this");
-        _;
-    }
-
-    // Simple business registration function - just stores the business name
-    function registerBusiness(string memory businessName) public {
-        // Store the business name on the blockchain
-        registeredBusinessNames[msg.sender] = businessName;
+    
+    // Business registration function
+    function registerBusiness(string memory _businessName) public {
+        require(!isRegisteredBusiness[msg.sender], "Business already registered");
+        require(bytes(_businessName).length > 0, "Business name cannot be empty");
+        
         isRegisteredBusiness[msg.sender] = true;
+        businessNames[msg.sender] = _businessName;
+        registeredBusinesses.push(msg.sender);
         
-        // Make the business a minter automatically
-        minters[msg.sender] = true;
-        
-        emit BusinessRegistered(msg.sender, businessName);
+        emit BusinessRegistered(msg.sender, _businessName);
     }
-
+    
     // Check if a business is registered
-    function isBusinessRegistered(address businessAddress) public view returns (bool) {
-        return isRegisteredBusiness[businessAddress];
+    function checkBusinessRegistration(address _businessAddress) public view returns (bool, string memory) {
+        return (isRegisteredBusiness[_businessAddress], businessNames[_businessAddress]);
+    }
+    
+    // Issue voucher function - only registered businesses can issue vouchers
+    function issueVoucher(address _customer, string memory _description, uint _value) public returns (uint) {
+        require(isRegisteredBusiness[msg.sender], "Only registered businesses can issue vouchers");
+        require(_customer != address(0), "Invalid customer address");
+        require(_value > 0, "Value must be positive");
+        require(bytes(_description).length > 0, "Description cannot be empty");
+
+        uint voucherId = customerVoucherCount[_customer];
+        customerVouchers[_customer][voucherId] = Voucher({
+            id: voucherId,
+            description: _description,
+            value: _value,
+            used: false,
+            business: msg.sender,
+            issueDate: block.timestamp
+        });
+        
+        customerVoucherCount[_customer]++;
+        emit VoucherIssued(msg.sender, _customer, voucherId, _value);
+        return voucherId;
+    }
+    
+    // Use voucher function
+    function useVoucher(uint _voucherId) public {
+        require(_voucherId < customerVoucherCount[msg.sender], "Voucher doesn't exist");
+        require(!customerVouchers[msg.sender][_voucherId].used, "Voucher already used");
+        
+        customerVouchers[msg.sender][_voucherId].used = true;
+        emit VoucherUsed(msg.sender, _voucherId);
+    }
+    
+    // Get voucher details
+    function getVoucherDetails(address _customer, uint _voucherId) public view 
+        returns (string memory description, uint value, bool used, address business, uint issueDate) {
+        require(_voucherId < customerVoucherCount[_customer], "Voucher doesn't exist");
+        
+        Voucher memory voucher = customerVouchers[_customer][_voucherId];
+        return (voucher.description, voucher.value, voucher.used, voucher.business, voucher.issueDate);
     }
 
-    // Get business name
-    function getBusinessName(address businessAddress) public view returns (string memory) {
-        return registeredBusinessNames[businessAddress];
+    // Create a public voucher (only businesses)
+    function createPublicVoucher(string memory _description, uint _value, uint _supply) public {
+        require(isRegisteredBusiness[msg.sender], "Only registered businesses");
+        require(_value > 0, "Value must be positive");
+        require(_supply > 0, "Supply must be positive");
+        require(bytes(_description).length > 0, "Description cannot be empty");
+
+        uint voucherId = publicVouchers.length;
+        publicVouchers.push(PublicVoucher({
+            id: voucherId,
+            description: _description,
+            value: _value,
+            business: msg.sender,
+            totalSupply: _supply,
+            claimedCount: 0,
+            isActive: true
+        }));
+
+        emit PublicVoucherCreated(voucherId, msg.sender, _description, _value);
     }
 
-    // Original functions
-    function earnPoints(address recipient, uint amount) public onlyMinter {
-        balanceOf[recipient] += amount;
-        totalSupply += amount;
+    // Claim a public voucher
+    function claimPublicVoucher(uint _voucherId) public {
+        require(_voucherId < publicVouchers.length, "Invalid voucher");
+        PublicVoucher storage voucher = publicVouchers[_voucherId];
+        
+        require(voucher.isActive, "Voucher not active");
+        require(!hasClaimedVoucher[_voucherId][msg.sender], "Already claimed");
+        require(voucher.claimedCount < voucher.totalSupply, "Voucher supply exhausted");
+
+        hasClaimedVoucher[_voucherId][msg.sender] = true;
+        voucher.claimedCount++;
+
+        uint customerVoucherId = customerVoucherCount[msg.sender];
+        customerVouchers[msg.sender][customerVoucherId] = Voucher({
+            id: customerVoucherId,
+            description: voucher.description,
+            value: voucher.value,
+            used: false,
+            business: voucher.business,
+            issueDate: block.timestamp
+        });
+        customerVoucherCount[msg.sender]++;
+
+        emit VoucherClaimed(msg.sender, _voucherId);
+        emit VoucherIssued(voucher.business, msg.sender, customerVoucherId, voucher.value);
     }
 
-    function setMinter(address account, bool isMinter) public onlyAdmin {
-        minters[account] = isMinter;
+    // Get all active public vouchers
+    function getActivePublicVouchers() public view returns (PublicVoucher[] memory) {
+        uint activeCount = 0;
+        for (uint i = 0; i < publicVouchers.length; i++) {
+            if (publicVouchers[i].isActive && publicVouchers[i].claimedCount < publicVouchers[i].totalSupply) {
+                activeCount++;
+            }
+        }
+
+        PublicVoucher[] memory activeVouchers = new PublicVoucher[](activeCount);
+        uint index = 0;
+        for (uint i = 0; i < publicVouchers.length; i++) {
+            if (publicVouchers[i].isActive && publicVouchers[i].claimedCount < publicVouchers[i].totalSupply) {
+                activeVouchers[index] = publicVouchers[i];
+                index++;
+            }
+        }
+        return activeVouchers;
     }
 
-    function mint(address to, uint amount) public onlyMinter {
-        balanceOf[to] += amount;
-        totalSupply += amount;
-    }
-
-    function transfer(address to, uint amount) public returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Not enough tokens");
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-        return true;
+    // Get total number of registered businesses
+    function getBusinessCount() public view returns (uint) {
+        return registeredBusinesses.length;
     }
 }
